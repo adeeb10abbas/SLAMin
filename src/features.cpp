610df4d300350fb3fc099ba67cd5b2305215b8b9
adeeb10abbas <aa4239@drexel.edu>
2022-03-13 00:40:50 -0500

    some more updates

--- src/features.cpp
+++ src/features.cpp
@@ -1,26 +1,24 @@
 #include "iostream"
 #include "features.hpp"
 #include <math.h>
+#include <pcl/io/pcd_io.h>
+# include <Eigen/Core>
+
 using namespace cv;
-/**
- * @brief Takes in a frame from a video and computes the keypoint 
- * and descripter using the FAST algorithm. Check - ]
- * https://docs.opencv.org/4.x/df/dd2/tutorial_py_surf_intro.html 
- * 
- * @param image1 
- * @return void 
- */
+using namespace Eigen;
 
 std::vector<cv::KeyPoint> featureProcessing::processImage(Mat& image){
     std::vector<cv::Point2f> corners;
     Mat descriptors;
     cv::goodFeaturesToTrack(image, corners, 3000, 0.01, 4);
+    // Add some documentation about what these args mean.
     vector<cv::Point2f> src_pts, dst_pts;
     // Generate ORBs from keypoints
     std::vector<cv::KeyPoint> keypoints;
-    cv::KeyPoint::convert(corners, keypoints); 
 
-    detector_->compute(image, keypoints, descriptors); // calculate descriptors from keypoints here
+    cv::KeyPoint::convert(corners, keypoints); 
+    // calculate descriptors from keypoints here
+    detector_->compute(image, keypoints, descriptors);
     
     std::vector<vector<DMatch>> matches;
     std::vector<DMatch> good_matches;
@@ -44,10 +42,11 @@ std::vector<cv::KeyPoint> featureProcessing::processImage(Mat& image){
     prev_.kps = keypoints;
     prev_.desc = descriptors; 
 
-    
     return keypoints;
   }
-//points 1 and point 2 would be - prev_.keypoints and keypoints 
+//points 1 and point 2 would be - prev_.keypoints and keypoints
+//TODO:There's probably a better way to do this. 
+
 Mat featureProcessing::calculateCamMat (const Mat& image, std::vector<Point2f> src_pts,
                     std::vector<Point2f> dst_pts) {
  // Calculating the middle of the image.
@@ -64,13 +63,21 @@ Mat featureProcessing::calculateCamMat (const Mat& image, std::vector<Point2f> s
           src_pts, dst_pts, // matching points
           cv::Mat_<double>::eye(3,3),
           cv::RANSAC, 0.999, 1.0, mask); // distance
-    cout <<  E << endl;
+    // cout <<  E << endl;
   
   // Recovering Pose
   cv::Mat R, t;
+  
   cv::recoverPose(E, src_pts, dst_pts, cv::Mat_<double>::eye(3,3), R, t, mask);
   // We get the rotation and translation out of here.
   
+  cout << "R: " << R << endl;
+  // Mat b= Mat::zeros();
+  Mat I = Mat_<double>::eye(3,3);
+  auto r_ = R*I;
+  cout << "R: " << r_ << endl;
+
+  // auto R_ = Eigen::Matrix4d::Identity();
   
 return E;
 }
@@ -83,36 +90,17 @@ return E;
 /* What I want to do today - 
 1) Figure out what to do with the Fundamental Matrix and the Mask
 2) Implement the whole thing
-3) Work on Visualization of 3d points in Open3D
+3) Work on Visualization of 3d points in PCL
 */
 
 // A function to draw some bs from the fundamental matrix? 
 static void Draw(string window_name, int height, int width, int left, int top){
-// const double max_z = 1.0e4;
-// 	for (int y = 0; y < xyz.rows; y++)
-// 	{
-// 		for (int x = 0; x < xyz.cols; x++)
-// 		{
-// 			Vec3f point = xyz.at<Vec3f>(y, x);
-// 			if (fabs(point[2] - max_z) < FLT_EPSILON || fabs(point[2]) > max_z) continue;
-
-// 			double X = point[0];
-// 			double Y = point[1];
-// 			double Z = point[2];
-
-// 			//pcl::PointXYZRGB rgbPoint;
-// 			pcl::PointXYZ rgbPoint;
-// 			rgbPoint.x = X;
-// 			rgbPoint.y = Y;
-// 			rgbPoint.z = Z;
-
-// 			//Vec3b colorLeft = img1.at<Vec3b>(y, x);
-
-// 			//uint32_t rgb = (static_cast<uint32_t>(colorLeft.val[2]) << 16 |
-// 			//	static_cast<uint32_t>(colorLeft.val[1]) << 8 | static_cast<uint32_t>(colorLeft.val[0]));
-// 			//rgbPoint.rgb = *reinterpret_cast<float*>(&rgb);
-
-// 			point_cloud->points.push_back(rgbPoint);
-// 		}
-// 	}
+const double max_z = 1.0e4;
+	for (int y = 0; y < 5; y++)
+	{
+		for (int x = 0; x < 5; x++)
+		{
+      pcl::PointXYZ min_point_AABB;
+		}
+	}
 }
